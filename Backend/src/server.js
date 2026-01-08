8086d648a5ed46b905ecf1440582b6b3dd672c9b
Kelvin Enobie <melvinenobie@gmail.com>
2026-01-08 01:17:09 -0600

    feat: added RESTful API endpoints for unassigned locations

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -72,6 +72,9 @@ app.post('/register', async (req, res) => {
   }
 });
 
+app.get('/login', (req, res) => {
+  res.status(200).send("Login endpoint is working");
+});
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
